8936eafdfc45ef157457c0cf491ab3589c879b74
Patryk Nowak <patryk@synaptek.ninja>
2022-05-11 13:58:43 +0100

    section 6

---
+++ Section_06_Problem_Solving_Exercises/multiple_pointerns_pattern.js
@@ -0,0 +1,26 @@
+// Naive solution (O)n2
+function sumZero(arr){
+    for(let i = 0; i < arr.length; i++){
+        for(let j = i+1; j < arr.length; j++){
+            if(arr[i] + arr[j] === 0){
+                return [arr[i], arr[j]];
+            }
+        }
+    }
+}
+
+
+sumZero([-4,-3,-2,-1,0,1,2,5])
+
+function countUniqueValues(arr){
+    if(arr.length === 0) return 0;
+    var i = 0;
+    for(var j = 1; j < arr.length; j++){
+        if(arr[i] !== arr[j]){
+            i++;
+            arr[i] = arr[j]
+        }
+    }
+    return i + 1;
+}
+countUniqueValues([1,2,2,5,7,7,99])
