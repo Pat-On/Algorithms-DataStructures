83e42992e06082bc1b8120043289783845f715a0
Patryk Nowak <patryk.r.nowak@gmail.com>
2022-05-26 14:44:03 +0100

    PQ

---
+++ Section_24_Binary_Heaps/Big(O)/notes.js
@@ -0,0 +1,3 @@
+// "Binary heaps are not ideal for searching that is why big (o) n";
+
+// " on average is big O (n/2)"
---
+++ Section_24_Binary_Heaps/Priority_queue/Priority_Queue_notes.js
@@ -0,0 +1,13 @@
+/*
+
+                Building a priority queue
+
+                What is a priority queue?
+
+    A data structure where each element has a priority.
+    Elements with higher priorities are served before 
+    elements with lower priorities.
+    
+
+    it is not bind exactly to the binary heap, just it is general abstract
+*/
