acf854a8b8c93bf8b6ecf8390048dffe65e3ed9f
Patryk Nowak <patryk.r.nowak@gmail.com>
2022-06-04 22:43:49 +0100

    dynamic programming = divide & conquer + memoization

---
+++ Udemy_Master_the_Data_Structures_and_algorithms/15_algorithms_Dynamic_programming/notes.js
@@ -0,0 +1,24 @@
+/*
+
+            Dynamic Programming
+
+    It is an optimization technique
+
+    Do You have something you can cache?
+
+    Dynamic Programming - it is just buzz words - it meaningless buzz word
+
+    this all is just related to the caching some values / results of some proccessing. 
+    so solve problems one and then do not repeat it!
+
+
+
+    **************************************************************************************************************
+
+                                    Memoization ~~~~ Caching
+
+
+
+
+
+*/
