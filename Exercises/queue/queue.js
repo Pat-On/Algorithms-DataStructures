1d14aa31f9706f6ed2d564c8b6c3522d8f750132
Patryk Nowak <patryk.r.nowak@gmail.com>
2022-06-19 09:58:11 +0100

    queue - naive approach - good for start during the interview

---
+++ Exercises/queue/queue.js
@@ -0,0 +1,22 @@
+const breaker = require("../breaker_line")();
+
+breaker();
+
+class Queue {
+  constructor() {
+    this.queue = [];
+  }
+
+  add(element) {
+    return this.queue.unshift(element);
+  }
+
+  remove() {
+    return this.queue.pop();
+  }
+}
+
+const q = new Queue();
+console.log(q.add(1));
+
+console.log(q.remove());
