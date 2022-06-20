96860b13d0e911dd0be9aa30c576a6a176102b52
Patryk Nowak <patryk.r.nowak@gmail.com>
2022-06-20 08:01:44 +0100

    stack

---
+++ Exercises/stack/stack.js
@@ -0,0 +1,21 @@
+const breaker = require("../breaker_line")();
+
+breaker();
+
+class Stack {
+  constructor() {
+    this.data = [];
+  }
+
+  push(item) {
+    return this.data.push(item);
+  }
+
+  pop() {
+    return this.data.pop();
+  }
+
+  peek() {
+    return this.data[this.data.length - 1];
+  }
+}
