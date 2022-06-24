2166b7423f0c9efbc04fec233c27a59142cea48d
Patryk Nowak <patryk.r.nowak@gmail.com>
2022-06-24 07:13:21 +0100

    fast and slow pointer - linked list

---
+++ Exercises/midPoint/midPoint.js
@@ -0,0 +1,46 @@
+const breaker = require("../breaker_line")();
+const { LinkedList } = require("../linkedList/index");
+
+/* 
+return the middle node of a linked list.
+If the list has an even number of elements, return 
+the node at the end of the first half of the list.
+Do not use a counter variable, do not retrieve 
+the size of the list and only iterate through the list one time
+
+-- Example
+const l = LinkedList()
+l.insertLast("a")
+l.insertLast("b")
+l.insertLast("c")
+
+midpoint(l) // returns {data: 'b'}
+
+*/
+
+function midpoints(list) {
+  let slow = list.getFirst();
+  let fast = list.getFirst();
+
+  while (fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return slow;
+}
+
+breaker();
+
+const list = new LinkedList();
+
+list.insertLast(1);
+list.insertLast(2);
+list.insertLast(3);
+
+console.log(midpoints(list));
+
+breaker();
+list.insertLast(4);
+
+console.log(midpoints(list));
