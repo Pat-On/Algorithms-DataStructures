b5e5364b2200e9937e4cb90d0be2cf661a4d068c
Patryk Nowak <patryk.r.nowak@gmail.com>
2022-06-25 10:31:27 +0100

    trees' node

---
+++ Exercises/trees/trees.js
@@ -0,0 +1,24 @@
+const breaker = require("../breaker_line")();
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
+
+  add(data) {
+    this.children.push(new Node(data));
+  }
+
+  remove(data) {
+    this.children.filter((node) => {
+      return node.data !== data;
+    });
+  }
+}
+
+class Tree {
+  constructor() {}
+}
+
+breaker();
