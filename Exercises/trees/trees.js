258020b6b07fc721c03521d8f0cb9086acc0eb74
Patryk Nowak <patryk.r.nowak@gmail.com>
2022-06-26 06:09:29 +0100

    traverse DF and BF

--- Exercises/trees/trees.js
+++ Exercises/trees/trees.js
@@ -18,7 +18,39 @@ class Node {
 }
 
 class Tree {
-  constructor() {}
+  constructor() {
+    this.root = null;
+  }
+
+  traverseBF(fn) {
+    const arr = [this.root]; // linked list would be faster
+
+    while (arr.length) {
+      const node = arr.shift(); // shift - always first element
+
+      arr.push(...node.children); // "looping"
+      fn(node);
+    }
+  }
+
+  traverseDF(fn) {
+    const arr = [this.root]; // linked list would be faster
+
+    while (arr.length) {
+      const node = arr.shift(); // shift - always first element
+
+      arr.unshift(...node.children); // <-- difference - unshift has horrible performance
+      fn(node);
+    }
+  }
+  /*
+ Implement traverseBF and traverseDF on the tree class.
+ each method should accept a function that gets called with each element in the tree
+
+    BF - Breadth-First Traversal - level by level 
+    DF - Deep-First Traversal - from very root to the bottom! 
+
+ */
 }
 
 breaker();
