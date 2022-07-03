b2699f0b36b3c1548075ad716e0fe8f6b3af991c
Patryk Nowak <patryk.r.nowak@gmail.com>
2022-07-03 19:31:13 +0200

    trapping water

---
+++ Katas/Arrays/q3TrappingRainwater/trappingRainwater.js
@@ -0,0 +1,32 @@
+/* 
+    Given an array of integers representing an
+    elevation map where the width of each bar is 1,
+    return how much rainwater can be trapped.
+
+    [0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2]
+
+    the only distance between elements when the element value
+    has 0. 
+
+    Step 1: Verify the constraints
+
+        Do the left and right sides of the graph count as walls
+            - No, the sides are not walls
+
+        Will there be negative integers
+            - No, assume all integers are positive
+
+     
+    Step 2: Write out some test cases
+
+        [0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2]   Answer: 8
+        []          Answer 0
+        [3]         Answer 0
+        [3,4, 3]    Answer 0
+
+    Step 3: Figure out a solution without code
+
+        
+        
+
+*/
