e47a0f0b2168aff19d0b5b926e3dc9f5852b8882
Patryk Nowak <patryk.r.nowak@gmail.com>
2022-07-18 18:13:19 +0200

    repeating longest substring improved solution

--- Katas/Strings/q2LongestSubstringwithoutRepeatingCharacters/longestSubstring-improved.js
+++ Katas/Strings/q2LongestSubstringwithoutRepeatingCharacters/longestSubstring-improved.js
@@ -1,22 +1,37 @@
-const string = "au";
+const string = "autgdfh";
 
 const lengthOfLongestSubstring = function (s) {
   if (s.length <= 1) return s.length;
 
-  const seen = {};
-  let left = 0,
-    longest = 0;
+  // setting variables
+  // const seen = {};
+  const seen = new Map();
+
+  // optimized version - map in javascript
+  let left = 0;
+  let longest = 0;
 
   for (let right = 0; right < s.length; right++) {
     const currentChar = s[right];
-    const previouslySeenChar = seen[currentChar];
+    // const previouslySeenChar = seen[currentChar];
 
+    // map syntax
+    const previouslySeenChar = seen.get(currentChar);
+    // comparing if it is more than left boundary
     if (previouslySeenChar >= left) {
+      //we are moving left pointer if the letter repeated
       left = previouslySeenChar + 1;
     }
 
-    seen[currentChar] = right;
+    // resetting current value inside object
+    // seen[currentChar] = right;
 
+    //map syntax
+    seen.set(currentChar, right);
+
+    // choosing the longest substring  (+1 because we want to know how many element
+    // is from - to including elements from and to
+    // we want to count elements not a spaces between :>
     longest = Math.max(longest, right - left + 1);
   }
 
