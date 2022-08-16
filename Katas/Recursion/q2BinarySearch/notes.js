67d65c9a483f19aa7eb2994828676e6248b3af35
Patryk Nowak <patryk.r.nowak@gmail.com>
2022-08-16 14:07:19 +0100

    notes linear and binary search

---
+++ Katas/Recursion/q2BinarySearch/notes.js
@@ -0,0 +1,17 @@
+/*
+    Small Recap:
+        Linear Search of course Time: O(n)
+        [1, 2, 3, 4, 5, 6, 7]
+        7 - you need to check all elements
+
+
+
+    Binary Search Time O(log n)
+        [1, 2, 3, 4, 5, 6, 7]
+idx      0, 1, 2, 3, 4, 5, 6
+
+    L: 0     5          R: 6
+
+    mid = (L+R) /2 <-- lowing it down in our example
+
+*/
