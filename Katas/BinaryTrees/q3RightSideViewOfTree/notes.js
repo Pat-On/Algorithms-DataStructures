215a23f56b5cc612550c19112589084cdce01909
Patryk Nowak <patryk.r.nowak@gmail.com>
2022-08-19 07:14:26 +0100

    notes q3 binary trees

---
+++ Katas/BinaryTrees/q3RightSideViewOfTree/notes.js
@@ -0,0 +1,31 @@
+/*
+    Given a binary tree, imagine you are standing
+    to the right of the tree. 
+    return an array of the values of the nodes you 
+    can see ordered from top to bottom
+
+    Step 1: Verify the constraints
+
+        The same like in the a2 binary trees
+
+    Step 2: Write out some test cases
+
+                    3
+                  /   \
+                6        1
+               /  \    /  \                      o
+            9      2        4               >   /|\
+          /  \                                   ^
+               5
+              /  \
+            8
+
+    answer: [1, 3, 6, 7, 8]
+
+
+    null answer: []
+
+
+                    3
+                  /   \     answer: [3]
+*/
