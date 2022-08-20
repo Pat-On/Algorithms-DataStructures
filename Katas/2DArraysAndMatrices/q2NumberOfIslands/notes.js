c5a6cc810953ec03107b71ed04e1caa052b7685c
Patryk Nowak <patryk.r.nowak@gmail.com>
2022-08-20 21:58:26 +0100

    notes about approaching 2-d array's katas

---
+++ Katas/2DArraysAndMatrices/q2NumberOfIslands/notes.js
@@ -0,0 +1,47 @@
+/*
+                2-D Arrays Approach
+
+                row    --                 --
+            0  | [x, x, x, x, x, x] |               6 x 6 
+            1  | [x, x, x, x, x, x] |
+            2  | [x, x, x, x, x, x] |
+            3  | [x, x, x, x, x, x] |
+            4  | [x, x, x, x, x, x] |
+            5  | [x, x, x, x, x, x] |
+            --                  --
+
+    Values follow to patterns
+        - limited (very specific values example A B C)
+
+        - unlimited (can be practically anything)
+
+    
+    We have DFS and BFS in 2-D arrays to. 
+
+    Sequential order:
+        from left to right
+        to the begging of other line
+        from left to right
+
+
+
+    ----------------- Limited Values -----------------
+
+
+    IS there a relationship between the values
+        - in our example between start and end there is relationship:
+            + we need to get from start to end
+        - there is relationship between green and blue values which are walls and you cannot cross them
+
+    Is there repeated subproblem that repeated would solve the problem in general4
+        - example: 
+            + we can solve the problem for only one element start
+
+---------------------------------------------------------------------------------------
+---------------------------------- Number Of Islands ----------------------------------
+---------------------------------------------------------------------------------------
+
+
+
+
+*/
