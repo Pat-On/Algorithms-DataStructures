099a2eb97b5bc2b652db24aaff4fe792bcd9cf55
Patryk Nowak <patryk.r.nowak@gmail.com>
2022-08-29 17:46:45 +0100

    kata: solve sudoku

---
+++ Katas/Backtracking/notes.js
@@ -0,0 +1,40 @@
+/*
+
+            Backtracking is solving similar problems to dynamic programing,
+
+                the key difference is that backtracking need to:
+
+                    Return ALL solutions
+                            or
+                    Return a VALID solution amongst all solutions
+
+
+            ----------- Backtracking ---------
+                            Kata
+
+
+            Create a function that solves for any 9x9 sudoku puzzle given.
+
+                Each of the digits 1-9 must occur exactly once in each row.
+
+                Each of the digits 1-9 must occur exactly once in each column.
+
+                Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+
+
+        Step 1: Verify the constraints
+
+            What happens if the board cannot be solved?
+                - Leave the sudoku puzzle as is.
+
+        Step 2: Write out some test cases
+
+            because the examples are time consuming
+            you may think about base case
+            and some examples that are invalid
+
+
+
+
+
+*/
