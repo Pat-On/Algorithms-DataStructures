c360e02bb81e668a47bec9ff907516e9ea8c8f61
Patryk Nowak <patryk.r.nowak@gmail.com>
2022-09-06 08:59:30 +0100

    steps recursive solution

--- Katas/Mix_Playground_LeetCode/steps.js
+++ Katas/Mix_Playground_LeetCode/steps.js
@@ -23,3 +23,29 @@ steps(4);
 //    '##  '
 //    '### '
 //    '####'
+
+// recursive solution
+function stepsRec(n, row = 0, stair = "") {
+  // base case
+  if (n === row) {
+    return;
+  }
+
+  // next level
+  if (stair.length === n) {
+    console.log(stair);
+    return stepsRec(n, row + 1);
+  }
+
+  // current level building process
+  if (stair.length <= row) {
+    stair += "#";
+  } else {
+    stair += " ";
+  }
+  stepsRec(n, row, stair);
+}
+console.log("Recursion");
+stepsRec(3);
+stepsRec(10);
+stepsRec(20);
