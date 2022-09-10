e8581bad54697043b3ca9f623376ab929defdf36
Patryk Nowak <patryk.r.nowak@gmail.com>
2022-09-10 18:17:18 +0100

    git merge

--- Katas/Mix_Playground_LeetCode/sorts/mergeSort/mergeSort.js
+++ Katas/Mix_Playground_LeetCode/sorts/mergeSort/mergeSort.js
@@ -1,5 +1,27 @@
-function mergeSort(arr) {}
+function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
 
-function merge(left, right) {}
+  const halfLength = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, halfLength));
+  const right = mergeSort(arr.slice(halfLength));
 
-console.log(bubbleSort([100, -40, 500, -124, 0, 21, 7]));
+  return merge(left, right);
+}
+
+function merge(left, right) {
+  const result = [];
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
+    }
+  }
+
+  return result.concat(right).concat(left);
+}
+
+console.log(mergeSort([100, -40, 500, -124, 0, 21, 7]));
