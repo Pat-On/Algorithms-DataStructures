7122c7b7bcbfd6542551ca9cd0e07b2582358218
Patryk Nowak <patryk.r.nowak@gmail.com>
2022-10-04 21:33:11 +0100

    container with the most water

---
+++ LeetCode/containerWithTheMostWater/javascriptSolution.js
@@ -0,0 +1,38 @@
+const maxArea = function (height) {
+  let left = 0;
+  let right = height.length - 1;
+  let amount = 0;
+
+  // let leftToReturn = left
+  // let rightToReturn = right
+
+  while (left < right) {
+    let amountToCompare = 0;
+
+    if (height[left] <= height[right]) {
+      amountToCompare = height[left] * (right - left);
+    } else {
+      amountToCompare = height[right] * (right - left);
+    }
+
+    // console.log(left, right)
+    if (amountToCompare > amount) {
+      // leftToReturn = left
+      // rightToReturn = right
+      amount = amountToCompare;
+    }
+    // smaller left/right height * rightIdx - leftIdx
+
+    // move indexes
+    if (height[left] <= height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  // console.log(leftToReturn, rightToReturn)
+  // return the amount of water
+  return amount;
+};
+
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
---
+++ LeetCode/containerWithTheMostWater/pythonSolution.py
@@ -0,0 +1,29 @@
+def maxArea(height: list[int]) -> int:
+    left = 0
+    right = len(height) - 1
+    amount = 0
+    while left < right:
+        calcAmount = 0
+
+        # calculate amount
+        if height[left] <= height[right]:
+            calcAmount = height[left] * (right - left)
+        else:
+            calcAmount = height[right] * (right - left)
+        # change valuye of amount
+        if amount < calcAmount:
+            amount = calcAmount
+        print(left, right)
+
+        # move one of the pointers
+        if height[left] <= height[right]:
+            left += 1
+        else:
+            right -= 1
+
+        # value to return
+
+    return amount
+
+
+print(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
