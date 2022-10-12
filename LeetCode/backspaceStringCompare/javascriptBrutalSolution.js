a1bc32f45d814a9a81960e980ce29ea1cc1c8c8c
Patryk Nowak <patryk.r.nowak@gmail.com>
2022-10-12 14:13:40 +0100

    dry code javascriptBrutalSolution

--- LeetCode/backspaceStringCompare/javascriptBrutalSolution.js
+++ LeetCode/backspaceStringCompare/javascriptBrutalSolution.js
@@ -1,29 +1,23 @@
-const backspaceCompare = function (s, t) {
+const stringToArray = function (str) {
   let backspaceCount = 0;
-  let newS = [];
-  let newT = [];
-
-  for (let i = s.length - 1; i >= 0; i--) {
-    if (s[i] === "#") {
+  let arr = [];
+  for (let i = str.length - 1; i >= 0; i--) {
+    if (str[i] === "#") {
       backspaceCount++;
     } else if (backspaceCount === 0) {
-      newS.unshift(s[i]);
+      //   newS.unshift(s[i]);// no need for re-indexing
+      arr.push(str[i]);
     } else {
       backspaceCount--;
     }
   }
 
-  backspaceCount = 0;
+  return arr;
+};
 
-  for (let i = t.length - 1; i >= 0; i--) {
-    if (t[i] === "#") {
-      backspaceCount++;
-    } else if (backspaceCount === 0) {
-      newT.unshift(t[i]);
-    } else {
-      backspaceCount--;
-    }
-  }
+const backspaceCompare = function (s, t) {
+  let newS = stringToArray(s);
+  let newT = stringToArray(t);
 
   if (newS.length !== newT.length) {
     return false;
