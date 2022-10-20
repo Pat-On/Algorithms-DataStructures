2aea43d523882d267c87ec223bc003e194807f99
Patryk Nowak <patryk.r.nowak@gmail.com>
2022-10-20 19:26:27 +0100

    added reverse linked list

---
+++ LeetCode/reverseLinkedList/pythonBrutalSolution.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/reverse-linked-list-ii/
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        pass
